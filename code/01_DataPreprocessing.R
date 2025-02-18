80ab788590b2059a4136b3a3417e113d06dd2ebe
WenzyWong <wanjit.wong@outlook.com>
2025-02-18 14:16:07 +0800

    revise code for overlapped genus-sample matrix extraction

--- code/01_DataPreprocessing.R
+++ code/01_DataPreprocessing.R
@@ -15,26 +15,38 @@ DIR_TABLE <- "/data/yzwang/project/AEG_seiri/table_infos/"
 
 contaminants <- colnames(read.table(paste0(DIR_TABLE, "contaminants.txt"), header = T))
 ### GC ###
-# Using 16S genus-level abundance matrix to generate 16S-based abundance matrix
-abund_gc_16S <- as.data.frame(read_excel(paste0(DIR_TABLE, "GC_16S_genus_abundance.xlsx")))
-rownames(abund_gc_16S) <- gsub("g__", "", abund_gc_16S$Genus)
-abund_gc_16S <- abund_gc_16S[ , -1]
-abund_gc_16S <- abund_gc_16S[!rownames(abund_gc_16S) %in% contaminants, ]
-saveRDS(abund_gc_16S, paste0(DIR_RDS, "GC_16S_abundance.rds"))
+# Read and process 16S abundance data
+abund_16s <- read_excel(file.path(DIR_TABLE, "GC_16S_genus_abundance.xlsx")) |>
+  as.data.frame()
+rownames(abund_16s) <- gsub("g__", "", abund_16s$Genus)
+abund_16s <- abund_16s[, -1]
+abund_16s <- abund_16s[!rownames(abund_16s) %in% contaminants, ]
 
-# Using RNA-seq genus-level count matrix to generate RNA-based abundance matrix
-count_gc_RNA <- read.csv(paste0(DIR_TABLE, "GC_RNA_genus_count.csv"))
+# Calculate 16S CPM
+cpm_16s <- abund_16s * 1e4
+saveRDS(cpm_16s, file.path(DIR_RDS, "gGC_CPM_16S_WithoutCompFilt.rds"))
 
-tmp_split <- unlist(strsplit(count_gc_RNA$X, "\\|"))
-rownames(count_gc_RNA) <- gsub("g__", "", tmp_split[grep("g__", tmp_split)])
+# Read and process RNA-seq count data
+count_rna <- read.csv(file.path(DIR_TABLE, "GC_RNA_genus_count.csv"))
+rownames(count_rna) <- gsub("g__", "", unlist(strsplit(count_rna$X, "\\|"))[grep("g__", unlist(strsplit(count_rna$X, "\\|")))])
+count_rna <- count_rna[, -1]
+count_rna[is.na(count_rna)] <- 0
+count_rna <- count_rna[!rownames(count_rna) %in% contaminants, ]
+saveRDS(count_rna, file.path(DIR_RDS, "gGC_count_RNA.rds"))
 
-count_gc_RNA <- count_gc_RNA[ , -1]
-count_gc_RNA[is.na(count_gc_RNA)] <- 0
-count_gc_RNA <- count_gc_RNA[!rownames(count_gc_RNA) %in% contaminants, ]
-saveRDS(count_gc_RNA, paste0(DIR_RDS, "GC_RNA_count.rds")) # Save for calculating Shannon indexes
+# Calculate RNA-seq CPM
+cpm_rna <- t(t(count_rna) * 1e6 / colSums(count_rna))
+saveRDS(cpm_rna, file.path(DIR_RDS, "gGC_CPM_RNA_WithoutCompFilt.rds"))
 
-abund_gc_RNA <- count_gc_RNA
-for (i in 1:ncol(abund_gc_RNA)) {
-  abund_gc_RNA[ , i] <- (abund_gc_RNA[ , i] * 1e2) / colSums(count_gc_RNA)[i]
-}
-saveRDS(abund_gc_RNA, paste0(DIR_RDS, "GC_RNA_abundance.rds"))
+# Create comparable matrices
+common_rows <- sort(intersect(rownames(cpm_16s), rownames(cpm_rna)))
+common_cols <- sort(intersect(colnames(cpm_16s), colnames(cpm_rna)))
+
+comp_16s <- cpm_16s[common_rows, common_cols]
+comp_rna <- cpm_rna[common_rows, common_cols]
+
+dim(comp_16s)
+dim(comp_rna)
+
+saveRDS(comp_16s, file.path(DIR_RDS, "gGC_CPM_16S_comparison.rds"))
+saveRDS(comp_rna, file.path(DIR_RDS, "gGC_CPM_RNA_comparison.rds"))
