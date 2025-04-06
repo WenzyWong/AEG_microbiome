d3945091c10a197823641fba1089fb2f0270da36
WenzyWong <wanjit.wong@outlook.com>
2025-04-06 16:35:42 +0800

    draw bacterial cases

--- code/02_MethodComparison.R
+++ code/02_MethodComparison.R
@@ -70,3 +70,59 @@ p_overall_cor <-
 pdf(file.path(DIR_RES, "A_overall_correlation_16S_RNAseq.pdf"), width = 4, height = 4)
 print(p_overall_cor)
 dev.off()
+
+# Case: Helicobacter
+df_helicobacter <- data.frame(
+  row.names = colnames(cpm_16s),
+  method_16s = log2(as.numeric(cpm_16s["Helicobacter", ]) + 1), 
+  method_rna = log2(as.numeric(cpm_rna["Helicobacter", ]) + 1))
+
+cor_helicobacter <- psych::corr.test(df_helicobacter$method_16s, df_helicobacter$method_rna,
+                                     use = "pairwise", method = "spearman")
+p_density_cor_helicobacter <-
+  ggplot(df_helicobacter, aes(x = method_16s, y = method_rna)) +
+  geom_point(size = 1.2, colour = "grey80") +
+  geom_smooth(method = lm, linewidth = 2, color = "#F48118", fill = "grey80") +
+  stat_density2d(aes(colour = after_stat(level)), size = .5, alpha = 0.33) +
+  scale_color_gradient(low = '#4F94CD85', high = '#A52A2A85') +
+  ggtitle("Helicobacter - log2CPM") +
+  annotate("text", x = 1.5, y = 18, colour = 1,
+           label = paste0("Rs=",round(cor_helicobacter$r, 2),
+                          "\np",format.pval(cor_helicobacter$p.adj, 2))) +
+  xlab("RNA-seq") +
+  ylab("16S rRNA") +
+  theme_test() +
+  theme(panel.border = element_rect(fill = NA, colour = 1),
+        axis.text = element_text(colour = 1),
+        plot.title = element_text(face = "italic")) 
+pdf(file.path(DIR_RES, "B_correlation_density_case_helicobacter.pdf"), width = 4, height = 3.5)
+print(p_density_cor_helicobacter)
+dev.off()
+
+# Case: Haemophilus
+df_haemophilus <- data.frame(
+  row.names = colnames(cpm_16s),
+  method_16s = log2(as.numeric(cpm_16s["Haemophilus", ]) + 1), 
+  method_rna = log2(as.numeric(cpm_rna["Haemophilus", ]) + 1))
+
+cor_haemophilus <- psych::corr.test(df_haemophilus$method_16s, df_haemophilus$method_rna,
+                                    use = "pairwise", method = "spearman")
+p_density_cor_haemophilus <-
+  ggplot(df_haemophilus, aes(x = method_16s, y = method_rna)) +
+  geom_point(size = 1.2, colour = "grey80") +
+  geom_smooth(method = lm, linewidth = 2, color = "#F48118", fill = "grey80") +
+  stat_density2d(aes(colour = after_stat(level)), size = .5, alpha = 0.33) +
+  scale_color_gradient(low = '#4F94CD85', high = '#A52A2A85') +
+  ggtitle("Haemophilus - log2CPM") +
+  annotate("text", x = 1.5, y = 13.5, colour = 1,
+           label = paste0("Rs=",round(cor_haemophilus$r, 2),
+                          "\np",format.pval(cor_haemophilus$p.adj, 2))) +
+  xlab("RNA-seq") +
+  ylab("16S rRNA") +
+  theme_test() +
+  theme(panel.border = element_rect(fill = NA, colour = 1),
+        axis.text = element_text(colour = 1),
+        plot.title = element_text(face = "italic")) 
+pdf(file.path(DIR_RES, "B_correlation_density_case_haemophilus.pdf"), width = 4, height = 3.5)
+print(p_density_cor_haemophilus)
+dev.off()
