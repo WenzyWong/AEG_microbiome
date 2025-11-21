209c8d312392bea8db076091663fb8a1df6f2921
WenzyWong <wanjit.wong@outlook.com>
2025-11-21 13:25:22 +0800

    fix version bug

--- utils/gsea_enrich.R
+++ utils/gsea_enrich.R
@@ -4,8 +4,10 @@ gsea_enrich <- function(level, geneList, species) {
     library(pathview)
     library(dplyr)
     geneList <- sort(geneList, decreasing = T)
-    if (species == "Mm") {gmt <- read.gmt(paste0("/data/yzwang/reference/gsea/", level, ".v2025.1.Mm.symbols.gmt"))}
-    if (species == "Hs") {gmt <- read.gmt(paste0("/data/yzwang/reference/GSEA/", level, ".v2023.1.Hs.symbols.gmt"))}
+    if (species == "Mm") {gmt <- read.gmt(paste0("/data/yzwang/reference/gsea/", 
+                                                 level, ".v2025.1.Mm.symbols.gmt"))}
+    if (species == "Hs") {gmt <- read.gmt(paste0("/data/yzwang/reference/gsea/", 
+                                                 level, ".v2023.2.Hs.symbols.gmt"))}
     gsea <- GSEA(geneList = geneList, TERM2GENE = gmt,
                  minGSSize = 1, verbose = F, pvalueCutoff = 0.05)
     return(gsea)
--- utils/sort_gene.R
+++ utils/sort_gene.R
@@ -1,6 +1,6 @@
 sort_gene <- function(dedf) {
-  sortList <- dedf$logFC
-  names(sortList) <- dedf$gene
+  sortList <- dedf$log2FC
+  names(sortList) <- dedf$symbol
   sortList <- sort(sortList, decreasing = T)
   return(sortList)
 }
