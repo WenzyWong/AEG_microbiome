3035de9a19d4859b3072e16a419c32dfd558c4c8
WenzyWong <wanjit.wong@outlook.com>
2026-04-07 15:19:20 +0800

    add differential analysis

--- code/05_CrosstalkHost.R
+++ code/05_CrosstalkHost.R
@@ -19,6 +19,7 @@ DIR_RES <- "/data/yzwang/project/AEG_seiri/results/F3_crosstalk/"
 DIR_TAB <- "/data/yzwang/project/AEG_seiri/table_infos/"
 DIR_TOOL <- "/data/yzwang/git_project/AEG_microbiome/utils/"
 
+# Import and preprocess
 clinical <- readxl::read_excel(file.path(DIR_TAB, "AEG_clinical.xlsx"))
 dim(clinical)
 
@@ -55,3 +56,26 @@ saveRDS(mtx_htpm_filter, file.path(DIR_RDS, "hTumourTPM_filtered.rds"))
 
 mtx_cpm <- readRDS(file.path(DIR_RDS, "sAEG_CPM_RNA_FiltMyco.rds"))
 dim(mtx_cpm)
+
+#######################
+# Differential analysis
+source(file.path(DIR_TOOL, "run_deseq_by_sp.R"))
+target_sp <- read.csv(file.path(DIR_TAB, "Species_within_saving_module.csv"),
+                      row.names = 1)
+
+abund_target <- data.frame(
+  sample = colnames(mtx_hcount),
+  t(mtx_cpm[target_sp$Species, colnames(mtx_hcount)] / 1e+4)
+)
+
+all_de_results <- lapply(
+  target_sp$Species,
+  run_deseq_by_sp,
+  abund_df   = abund_target,
+  count_mtx  = mtx_hcount,
+  target_sp_df = target_sp
+)
+
+names(all_de_results) <- target_sp$Species
+
+all_de_results <- Filter(Negate(is.null), all_de_results)
