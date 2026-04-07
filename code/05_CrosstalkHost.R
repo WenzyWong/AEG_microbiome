4c75f2376614f97fc112abd4db46c6cd7526376d
WenzyWong <wanjit.wong@outlook.com>
2026-04-07 19:11:27 +0800

    change to tumour matrix

--- code/05_CrosstalkHost.R
+++ code/05_CrosstalkHost.R
@@ -9,10 +9,12 @@
 setwd("/data/yzwang/project/AEG_seiri/")
 
 library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(DESeq2)
 library(enrichplot)
 library(paletteer)
+library(patchwork)
 
 DIR_RDS <- "/data/yzwang/project/AEG_seiri/RDS/"
 DIR_RES <- "/data/yzwang/project/AEG_seiri/results/F3_crosstalk/"
@@ -57,25 +59,56 @@ saveRDS(mtx_htpm_filter, file.path(DIR_RDS, "hTumourTPM_filtered.rds"))
 mtx_cpm <- readRDS(file.path(DIR_RDS, "sAEG_CPM_RNA_FiltMyco.rds"))
 dim(mtx_cpm)
 
+mtx_hcount_filter <- readRDS(file.path(DIR_RDS, "hTumourCNT_filtered.rds"))
+mtx_htpm_filter <- readRDS(file.path(DIR_RDS, "hTumourTPM_filtered.rds"))
+
 #######################
 # Differential analysis
+tumour_samples <- colnames(mtx_hcount)[startsWith(colnames(mtx_hcount), "C")]
+
 source(file.path(DIR_TOOL, "run_deseq_by_sp.R"))
 target_sp <- read.csv(file.path(DIR_TAB, "Species_within_saving_module.csv"),
                       row.names = 1)
 
+
 abund_target <- data.frame(
-  sample = colnames(mtx_hcount),
-  t(mtx_cpm[target_sp$Species, colnames(mtx_hcount)] / 1e+4)
+  sample = tumour_samples,
+  t(mtx_cpm[target_sp$Species, tumour_samples] / 1e+4)
 )
 
 all_de_results <- lapply(
   target_sp$Species,
   run_deseq_by_sp,
-  abund_df   = abund_target,
-  count_mtx  = mtx_hcount,
+  abund_df     = abund_target,
+  count_mtx    = mtx_hcount[, tumour_samples],
   target_sp_df = target_sp
 )
 
 names(all_de_results) <- target_sp$Species
 
 all_de_results <- Filter(Negate(is.null), all_de_results)
+
+###############
+# GSEA analysis
+source(file.path(DIR_TOOL, "run_gsea_by_species.R"))
+source(file.path(DIR_TOOL, "extract_ranking_df.R"))
+source(file.path(DIR_TOOL, "draw_summary_ridgeplot.R"))
+
+all_gsea_results <- lapply(
+  names(all_de_results),
+  run_gsea_by_species,
+  de_results = all_de_results,
+  dir_rds    = DIR_RDS
+)
+names(all_gsea_results) <- names(all_de_results)
+
+# Save summary ridgeplots
+fig_w <- 15
+fig_h <- max(6, length(all_gsea_results) * 2.5)
+
+for (gene_set in c("hallmark", "kegg")) {
+  pdf(file.path(DIR_RES, paste0("summary_gsea_ridge_", gene_set, ".pdf")),
+      width = fig_w, height = fig_h)
+  print(draw_summary_ridgeplot(all_gsea_results, gene_set))
+  dev.off()
+}
